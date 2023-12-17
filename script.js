cb952660b52bd996c9907cf268f5341f9d48e0aa
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2023-12-17 10:04:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let ans = '';
+	
+	if(shortHex[0] != '#')
+	{
+		return '#'+shortHex[0]+shortHex[0]+shortHex[1]+shortHex[1]+shortHex[2]+shortHex[2];
+	}
+	else{
+		return shortHex[0]+shortHex[1]+shortHex[1]+shortHex[2]+shortHex[2]+shortHex[3]+shortHex[3]+
+	}		
 };
 
 // Do not change the code below.
